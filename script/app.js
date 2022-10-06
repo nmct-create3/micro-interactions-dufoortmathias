e5c44881fccbe6592bac81618581eec1e3e18617
dufoortmathias <mathias.dufoort@student.howest.be>
2022-10-06 14:54:52 +0200

    password toggle

--- script/app.js
+++ script/app.js
@@ -3,7 +3,13 @@ function handleFloatingLabel() {
 }
 
 function handlePasswordSwitcher() {
-    
+    const passwordInput = document.querySelector(".js-password");
+    const passwordToggle = document.querySelector(".js-checkbox");
+    const passwordOptions = ["password", "text"];
+
+    passwordToggle.addEventListener('change', function () {
+        passwordInput.type = passwordOptions[+this.checked];
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
